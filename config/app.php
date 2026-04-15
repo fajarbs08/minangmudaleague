e358e53be8aab7d39a2375e3a76b855602028a66
Fajar <fajar.koja2000@gmail.com>
2026-04-15 08:52:46 +0700

    chore: update app branding defaults

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Minang Muda League'),
 
     /*
     |--------------------------------------------------------------------------
