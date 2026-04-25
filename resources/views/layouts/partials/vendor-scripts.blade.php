f73738ecd59e398bc2792faad16bc45ae57371b3
Fajar <fajar.koja2000@gmail.com>
2026-04-25 13:45:36 +0700

    Fix dashboard preloader blocked by dragula
    Define the browser global alias before loading the dashboard bundles so dragula no longer crashes early and leaves the preloader stuck on screen.

--- resources/views/layouts/partials/vendor-scripts.blade.php
+++ resources/views/layouts/partials/vendor-scripts.blade.php
@@ -1,2 +1,7 @@
+<script>
+    if (typeof globalThis !== 'undefined' && typeof globalThis.global === 'undefined') {
+        globalThis.global = globalThis;
+    }
+</script>
 @vite(['resources/js/app.js', 'resources/js/layout.js'])
 @yield('scripts')
